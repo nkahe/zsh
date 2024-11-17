00afc3880ead1de700e46d5d41fe15eed859af9e
Henri K <12832691+nkahe@users.noreply.github.com>
2024-11-17 15:55:48 +0200

    neovim-switcher update

--- plugins/neovim-switcher.zsh
+++ plugins/neovim-switcher.zsh
@@ -7,7 +7,7 @@
 #  items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim")
 
 function nvims() {
-  items=("LazyVim" "Old config")
+  items=("LazyVim" "NvChad" "AstroNvim" "Old config" )
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"
@@ -16,12 +16,18 @@ function nvims() {
     config=""
   elif [[ $config == "Old config" ]]; then
     config='nvim-old'
+  elif [[ $config == "NvChad" ]]; then
+    config='nvchad'
+  elif [[ $config == "AstroNvim" ]]; then
+    config='astroNvim'
   fi
   NVIM_APPNAME=$config nvim $@
 }
 
 # If want to start by using alias.
-alias lvim="NVIM_APPNAME=LazyVim nvim $@"
+alias lvim="nvim $@"
 alias nvim-old="NVIM_APPNAME=nvim-old nvim $@"
+alias nvchad="NVIM_APPNAME=nvchad nvim $@"
+alias astro="NVIM_APPNAME=astroNvim nvim $@"
 
 # bindkey -s '^a' "nvims\n"
