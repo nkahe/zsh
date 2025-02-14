4fdf5ba4e5f58474a1836999ce38fb3bc8917de9
Henri K <12832691+nkahe@users.noreply.github.com>
2025-02-14 18:19:30 +0200

    file formatting

--- snippets/dnf.sh
+++ snippets/dnf.sh
@@ -2,6 +2,13 @@ if ! command -v dnf &>/dev/null; then
   return
 fi
 
+# shortcut "~repos"
+if [ "$ZSH_NAME" ]; then
+  if [[ -d /etc/yum.repos.d ]]; then
+    hash -d repos=/etc/yum.repos.d
+  fi
+fi
+
 alias copr="sudo dnf copr"    \
       inf="dnf info"      \
       ins="sudo dnf install"  \
@@ -11,37 +18,35 @@ alias copr="sudo dnf copr"    \
       se="dnf search --exclude '*i686'"  \
       up="sudo dnf upgrade"
 
-
 whyfile() {
-    package=$(rpm -qf $1 --qf "%{NAME}")
-    ret=$?
-    echo -e "Installed by package: $package\n"
-    (( $ret == 0 )) || return
-    dnf info $package
-    #whypkg $package
+  package=$(rpm -qf $1 --qf "%{NAME}")
+  ret=$?
+  echo -e "Installed by package: $package\n"
+  (( $ret == 0 )) || return
+  dnf info $package
+  #whypkg $package
 }
 
 whycmd() {
-    file=$(which $1)
-    (($? == 0)) || return
-    whyfile $file
+  file=$(which $1)
+  (($? == 0)) || return
+  whyfile $file
 }
 
 whypkg() {
-    if dnf repoquery --unneeded | grep -w "$1" > /dev/null; then
-        echo "Package $1 is orphaned"
-        return
-    fi
+  if dnf repoquery --unneeded | grep -w "$1" > /dev/null; then
+    echo "Package $1 is orphaned"
+  fi
 
-    # Check what depends on the package
-    echo "These depend on that:"
-    dnf repoquery --whatdepends $1 || \
+  # Check what depends on the package
+  echo "These depend on that:"
+  dnf repoquery --whatdepends $1 || \
 
-    # Check what provides the package
-    echo "Providers by:"
-    dnf repoquery --whatprovides $1
+  # Check what provides the package
+  echo "Providers by:"
+  dnf repoquery --whatprovides $1
 }
 
 pkgchg() {
-    rpm -q --changelog $1 | less
+  rpm -q --changelog $1 | less
 }
