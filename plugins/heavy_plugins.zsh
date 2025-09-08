a5310efd0e5bf7324d1091d888ccea8f35b91810
Henri K <12832691+nkahe@users.noreply.github.com>
2025-09-08 16:31:11 +0300

    Exclude slow Zinit -command for syntax highlighting.
    Run compinit and initialize completion if no syntax highlighting is
    being used.

--- plugins/heavy_plugins.zsh
+++ plugins/heavy_plugins.zsh
@@ -1,7 +1,9 @@
-# These plugins take some resources.
+# Load plugins which take some resources.
 
-# Skip these for weaker Raspberry. ID is set in /etc/os-release
+# Skip these for weaker systems and only run compinit. ID is set in /etc/os-release
 if [[ -n "$ID" && "$ID" == "raspbian" ]]; then
+  zicompinit
+  zicdreplay
   return
 fi
 
@@ -14,17 +16,26 @@ fi
 # NOTE: e.g., syntax highlighting must be loaded after executing compinit command
 # and sourcing other plugins (If the defer tag is given 2 or above, run after
 # compinit command)
+#
+# Compinit is called here!
+#
+# Unsetting "chroma-zinit" excludes "zinit" command's for highlighting because
+# it is very slow.
+#
+# TODO: If command below isn't included, compinit isn't called at all and
+# completion doesn't work.
 
 zinit wait silent for \
   atinit"ZINIT[COMPINIT_OPTS]=-C; \
     zicompinit; \
     zicdreplay; \
-      export region_highlight=''; \
+    export region_highlight=''; \
     (( $+commands[eza] )) && compdef eza=ls; \
     export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8' \
     ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd \
     ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=30" \
-    zdharma-continuum/fast-syntax-highlighting \
+    atload'unset "FAST_HIGHLIGHT[chroma-zinit]"' \
+    zdharma-continuum/fast-syntax-highlighting   \
   blockf \
     zsh-users/zsh-completions \
   atload"!_zsh_autosuggest_start" \
@@ -54,9 +65,9 @@ zinit wait silent for \
 # just typed. https://github.com/MichaelAquilina/zsh-you-should-use
 
 # NOTE: setaf 10 refers to color.
-zinit ice wait"2" lucid
-zinit light "MichaelAquilina/zsh-you-should-use"
-export YSU_MESSAGE_FORMAT="$(tput setaf 10)There is an alias for that: %alias$(tput sgr0)"
+# zinit ice wait"2" lucid
+# zinit light "MichaelAquilina/zsh-you-should-use"
+# export YSU_MESSAGE_FORMAT="$(tput setaf 10)There is an alias for that: %alias$(tput sgr0)"
 
 # Forked version of Prezto terminal. Use wait since this takes some time.
 # zinit ice wait lucid
