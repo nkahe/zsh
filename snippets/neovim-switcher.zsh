65a3520035c3648e7388727e103ddafdbea2e53c
Henri K <12832691+nkahe@users.noreply.github.com>
2025-10-30 09:23:30 +0200

    Update neovim-switcher.

--- snippets/neovim-switcher.zsh
+++ snippets/neovim-switcher.zsh
@@ -1,14 +1,16 @@
 
 # Pick an installed Neovim configuration to run from list using fzf.
 function nvims() {
-  items=("LazyVim" "Minimal" "NvChad" "AstroNvim" "SpaceVim" "Old config" "No plugins" "MiniMax" )
+  items=("Default" "Minimal" "NvChad" "AstroNvim" "SpaceVim" "Old config" "No plugins" "MiniMax" )
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"
     return 0
-  elif [[ $config == "LazyVim" ]]; then
+  elif [[ $config == "MiniMax" ]]; then
+    config='nvim-minimax'
+  elif [[ $config == "Default" ]]; then
     config=""
-  elif [[ $config == "Old config" ]]; then
+  elif [[ $config == "Custom config" ]]; then
     config='nvim-old'
   elif [[ $config == "NvChad" ]]; then
     config='nvchad'
@@ -18,8 +20,8 @@ function nvims() {
     config='SpaceVim'
   elif [[ $config == "No plugins" ]]; then
     config='nvim-no_plugins'
-  elif [[ $config == "MiniMax" ]]; then
-    config='nvim-minimax'
+  elif [[ $config == "Old config" ]]; then
+    config='nvim-old'
   fi
   NVIM_APPNAME=$config nvim $@
 }
@@ -31,4 +33,5 @@ alias nvchad="NVIM_APPNAME=nvchad nvim $@"
 alias astro="NVIM_APPNAME=astroNvim nvim $@"
 alias spacevim="NVIM_APPNAME=SpaceVim nvim $@"
 alias nvim-noplugins="NVIM_APPNAME=nvim-no_plugins nvim $@"
-alias minimax="NVIM_APPNAME=nvim-minimax nvim $@"
+alias mini="NVIM_APPNAME=nvim-minimax nvim $@"
+alias nvim-custom="NVIM_APPNAME=nvim-custom nvim $@"
