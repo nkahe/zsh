bd6543c233b7b07da7804056c08d10c888fca402
Henri K <12832691+nkahe@users.noreply.github.com>
2025-10-30 09:22:24 +0200

    Prefer bindkey -v over set -o vi.

--- plugins/prompt.zsh
+++ plugins/prompt.zsh
@@ -1,13 +1,23 @@
 
+# Select Vi input mode if it's set (in Zshrc).
+if [[ "$key_bindings" == vi ]]; then
+  # Zsh documentation recommends this over 'set -o vi'.
+  bindkey -v
+  # Set beam cursor.
+  print -n '\e[5 q'
+fi
+
 if [[ -n "$ID" && "$ID" == "raspbian" ]]; then
   # Zinit's updating below didn't work on Raspberry's executables so it's
   # updated manually.
   # zinit ice from"github-rel" bpick"*arm-unknown-linux-musleabihf*" as"program" \
+
   atload'!eval $(starship init zsh)' lucid
+  # Load Starhip theme if available.
   if (( $+commands[starship] )); then
     eval $(starship init zsh);
   else
-    # Backup -theme.
+    # Light custom backup -theme.
     themefile=$ZDOTDIR/themes/simple_theme.zsh
     [[ -f $themefile ]] && source $themefile
   fi
