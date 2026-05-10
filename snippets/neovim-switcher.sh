9a4e91a48a3de640df60f4454d955243f3678c8b
Henri K <12832691+nkahe@users.noreply.github.com>
2026-05-10 16:53:00 +0300

    formatting

--- snippets/neovim-switcher.sh
+++ snippets/neovim-switcher.sh
@@ -2,22 +2,26 @@
 # Pick an installed Neovim configuration to run from list using fzf with
 # "nvims" or individual functions names for different configs.
 
-if ! command -v nvim &>/dev/null; then
-  return
-fi
-
 function nvims() {
   if ! command -v fzf >/dev/null 2>&1; then
     echo "nvims: fzf is not installed" >&2
     return 1
   fi
+
   if ! command -v nvim >/dev/null 2>&1; then
     echo "nvims: nvim is not installed" >&2
     return 1
   fi
 
-  items=("Custom" "Lazyvim" "Default Lazyvim" "Kitty scrollback" "Minimal" "NvChad" "AstroNvim" "SpaceVim" "Old config" "No plugins" "MiniMax" "Testing")
-  config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+  items=(
+    "Custom" "Lazyvim" "Default Lazyvim" "Kitty scrollback" "Minimal" "NvChad" \
+    "AstroNvim" "SpaceVim" "Old config" "No plugins" "MiniMax" "Testing"
+  )
+
+  config=$( \
+    printf "%s\n" "${items[@]}" | \
+    fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0 \
+  )
 
   if [[ -z $config ]]; then
     echo "Nothing selected"
@@ -25,15 +29,15 @@ function nvims() {
   fi
 
   case "$config" in
-    Custom)  config="custom"  ;;
-    Lazyvim) config="lazyvim" ;;
-    MiniMax) config='minimax' ;;
-    NvChad)  config='nvchad'  ;;
-    Minimal) config='minimal' ;;
-    Testing) config='testing' ;;
-    SpaceVim)  config='SpaceVim'  ;;
-    AstroNvim) config='astroNvim' ;;
-    "Old config") config='old' ;;
+    Custom      ) config="custom"    ;;
+    Lazyvim     ) config="lazyvim"   ;;
+    MiniMax     ) config='minimax'   ;;
+    NvChad      ) config='nvchad'    ;;
+    Minimal     ) config='minimal'   ;;
+    Testing     ) config='testing'   ;;
+    SpaceVim    ) config='SpaceVim'  ;;
+    AstroNvim   ) config='astroNvim' ;;
+    "Old config") config='old'       ;;
     "No plugins") config='no_plugins'  ;;
     "Kitty scrollback") config="kitty" ;;
     "Default Lazyvim") config="lazyvim-default" ;;
@@ -51,14 +55,14 @@ function nvims() {
 
 # If want to start by using alias instead.
 mini() { NVIM_APPNAME="nvim/minimax" nvim "$@"; }
+astro() { NVIM_APPNAME="nvim/astroNvim" nvim "$@"; }
+nvchad() { NVIM_APPNAME="nvim/nvchad" nvim "$@"; }
 minimax() { NVIM_APPNAME="nvim/minimax" nvim "$@"; }
 lazyvim() { NVIM_APPNAME="nvim/lazyvim" nvim "$@"; }
-nvchad() { NVIM_APPNAME="nvim/nvchad" nvim "$@"; }
-astro() { NVIM_APPNAME="nvim/astroNvim" nvim "$@"; }
 spacevim() { NVIM_APPNAME="vim/spaceVim" nvim "$@"; }
-nvim-noplugins() { NVIM_APPNAME="nvim/no_plugins" nvim "$@"; }
+nvim-kitty() { NVIM_APPNAME="nvim/kitty" nvim "$@"; }
 nvim-custom() { NVIM_APPNAME="nvim/custom" nvim "$@"; }
 nvim-minimal() { NVIM_APPNAME="nvim/minimal" nvim "$@"; }
 nvim-testing() { NVIM_APPNAME="nvim/testing" nvim "$@"; }
-nvim-kitty() { NVIM_APPNAME="nvim/kitty" nvim "$@"; }
+nvim-noplugins() { NVIM_APPNAME="nvim/no_plugins" nvim "$@"; }
 lazyvim-default() { NVIM_APPNAME="nvim/lazyvim-default" nvim "$@"; }
